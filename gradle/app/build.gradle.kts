300d15182932cae07f3c29703f80ef76a943bc37
Joel Leblanc <leblanc.js@icloud.com>
2023-05-11 11:30:21 -0400

    Added a basic Gradle project with an app and a test.

---
+++ gradle/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    application
+}
+
+rootProject.group = "com.github.leblancjs.automatic_code_formatting_java"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+}
+
+application {
+    mainClass.set("com.github.leblancjs.automatic_code_formatting_java.gradle.App")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ gradle/app/src/test/java/com/github/leblancjs/automatic_code_formatting_java/gradle/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.leblancjs.automatic_code_formatting_java.gradle;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void isTrueReallyTrue() {
+        assertTrue(true);
+    }
+}
